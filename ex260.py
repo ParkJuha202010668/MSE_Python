4afc315e54db457a2d5b723971b4864fa02a8698
ParkJuha202010668 <ainks0002@jbnu.ac.kr>
2020-12-14 23:14:27 +0900

    added python files

---
+++ ex260.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+class OMG :
+    def print() : # class를 정의함   # def print(self)로 하면 Oh my god이 도출됨
+        print("Oh my god")
+        
+        
+myStock = OMG()    # OMG() 는 객체 생성
+myStock.print()    # OMG.print(myStock) .앞에 있는 것이 () 안으로 들어감.
+
+
+# In[ ]:
+
+
+
+
