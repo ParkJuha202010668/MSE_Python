b58d65b36763db408375c9aceb97e8ad44fc2317
ParkJuha202010668 <ainks0002@jbnu.ac.kr>
2020-12-14 23:06:31 +0900

    added python files

---
+++ ex090.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# Dictionery
+# 키(key)와 값(value)을 저장
+# {} 사용, :를 통해 구분
+
+
+icecream = {'폴라포': 1200, '빵빠레': 1800, '월드콘': 1500, '메로나': 1000}
+icecream['누가바']
+
+# Dictioner에 없는 keys를 사용해서 indexing하면 error가 발생함!
+
+
+# In[ ]:
+
+
+
+
