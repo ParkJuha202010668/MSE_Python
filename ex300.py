e9defef646970fff9df5725f0a552b602be4671c
ParkJuha202010668 <ainks0002@jbnu.ac.kr>
2020-12-14 23:16:01 +0900

    added python files

---
+++ ex300.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+per = ["10.31", "", "8.00"]
+
+# 파이썬 예외 처리
+
+for i in per:
+    try:    # 실행코드
+        print(float(i))
+    except:    # 예외가 발생했을 때 수행할 코드
+        print(0)
+    else:    # 예외가 발생하지 않았을 때 수행할 코드
+        print("clean data")
+    finally:    # 예외 발생 여부와 상관 없이 항상 수행할 코드
+        print("변환 완료")
+
+
+# In[ ]:
+
+
+
+
