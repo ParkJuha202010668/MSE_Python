043dcf8fcc9c9cfbfd7d1e7e340a3710d5215d82
ParkJuha202010668 <ainks0002@jbnu.ac.kr>
2020-12-14 23:05:24 +0900

    added python files

---
+++ ex060.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+nums = [1, 2, 3, 4, 5]
+average = sum(nums) / len(nums)
+print(average)
+
+# sum(nums)은 nums 리스트 안에 있는 원소들의 합
+# len(nums)은 nums 리스트 원소의 개수
+
