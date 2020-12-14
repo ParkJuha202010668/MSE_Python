0374d7a11a8adcdd451d0e8fd0c70eadf8544a36
ParkJuha202010668 <ainks0002@jbnu.ac.kr>
2020-12-14 23:03:20 +0900

    added python files

---
+++ ex030.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+string = 'abcd'
+string.replace('b', 'B') # 문자열은 변경할 수 없는 자료형
+print(string)
+
+# 원래 replace 멤버함수를 사용하면 원본은 그대로 두고 변경된 문자열 객체 교체함.
+
