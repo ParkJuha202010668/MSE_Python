8957ef59b152f84fc7b1e5b768f35bc4a9e68a57
ParkJuha202010668 <ainks0002@jbnu.ac.kr>
2020-12-14 23:08:55 +0900

    added python files

---
+++ ex150.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+리스트 = ["가", "나", "다", "라"]
+# 리스트에 4개의 문자열이 있음
+
+for 변수 in 리스트[: :-1]:
+    print(변수)
+    
+# 증감폭을 음수로 설정하면 뒤에서부터 값이 Slicing 됨
+
+
+# In[2]:
+
+
+# example
+a = 'Hello World'
+print(a[-1])
+
+
+# In[ ]:
+
+
+
+
