9deda0dbf26ce0b1d580429e41472b0f07823723
ParkJuha202010668 <ainks0002@jbnu.ac.kr>
2020-12-14 23:05:47 +0900

    added python files

---
+++ ex070.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+data = [2, 4, 3, 1, 5, 10, 9]
+data.sort()
+print(data)
+
+data = [2, 4, 3, 1, 5, 10, 9]
+data2 = sorted(data)
+print(data2)
+
+# 기본값은 False 작은수부터 정렬됨(오름차순)
+
