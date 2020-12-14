7bdf9488be45bb9db0e3e603b472cd6c0a555900
ParkJuha202010668 <ainks0002@jbnu.ac.kr>
2020-12-14 23:14:02 +0900

    added python files

---
+++ ex250.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# numpy 사용
+# arange 함수 사용
+# 0.0부터 5.0까지 0.1씩 증가시키기
+
+import numpy
+for i in numpy.arange(0, 5, 0.1):
+    print(i)
+    
+# numpy.arange() 함수는 range()함수와 유사한 기능
+# 대신 numpy.arange() 함수는 정수뿐만 아니라 소수 형태의 간격 지정이 가능
+# 별도로 numpy 라이브러리를 import 하는 번거로움이 있지만
+# 함수 그래프를 그릴 때나 촘촘하고 연속적인 값이 필요할 때 쓸 수 있음!1
+
+
+# In[ ]:
+
+
+
+
