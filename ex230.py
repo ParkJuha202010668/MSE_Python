e948dbbabc85352587206c414f904e7523cde644
ParkJuha202010668 <ainks0002@jbnu.ac.kr>
2020-12-14 23:13:11 +0900

    added python files

---
+++ ex230.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+def my_print (a, b) :
+    print("왼쪽:", a)
+    print("오른쪽:", b)
+    
+my_print(b=100, a=200) # 함수 호출할 때 변수 a에 200을 지정하고 변수 b에 100을 지정함.
+
+
+# In[ ]:
+
+
+
+
